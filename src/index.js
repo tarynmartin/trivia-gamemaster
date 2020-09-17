c661bf9c3adc38e93ae9b3004e241b99f8b527ba
Taryn Martin <taryn.martin919@gmail.com>
2020-09-17 14:11:29 -0600

    Add middleware to app for fetch and dev tools

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
-const store = createStore(rootReducer);
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(rootReducer, compose(applyMiddleware(thunk), devTools))
 
 ReactDOM.render(
   <Provider store={ store }>
