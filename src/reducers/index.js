50c1c4d2ff008420344a88cade61334aa4206fdc
Taryn Martin <taryn.martin919@gmail.com>
2020-09-17 18:23:18 -0600

    Add updateQuestions and addToGame to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import errorReducer from './error-reducer.js';
 import updateQuestionsReducer from './updateQuestions-reducer.js';
+import addToGameReducer from './addToGame-reducer.js'
 
 const rootReducer = combineReducers({
   errorMessage: errorReducer,
-  updateQuestions: updateQuestionsReducer
+  updateQuestions: updateQuestionsReducer,
+  addToGame: addToGameReducer
 });
 
 export default rootReducer;
