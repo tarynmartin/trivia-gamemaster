eb2d322455cbecaab54190141cf3f26c9dc27ff5
Taryn Martin <taryn.martin919@gmail.com>
2020-09-18 17:12:10 -0600

    Add case to reducer to allow a user to remove a question from store

--- src/reducers/addToGame-reducer.js
+++ src/reducers/addToGame-reducer.js
@@ -2,6 +2,12 @@ const addToGameReducer = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TO_GAME':
       return [...state, {id: action.id, question: action.question}];
+    case 'REMOVE_QUESTION':
+      return state.filter(question => {
+        if (question.id !== action.question.id) {
+          return question;
+        }
+      })
     default:
       return state;
   }
