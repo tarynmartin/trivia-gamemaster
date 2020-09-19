f80af541f6538ce78af050912d1c6f5ead08e43b
Taryn Martin <taryn.martin919@gmail.com>
2020-09-19 17:00:57 -0600

    Add preliminary tests

--- src/containers/TriviaCard/TriviaCard.test.js
+++ src/containers/TriviaCard/TriviaCard.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import TriviaCard from './TriviaCard.js';
-import { screen, render } from '@testing-library/react';
+import { TriviaCard } from './TriviaCard';
+import { screen, render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom'
 import { BrowserRouter } from 'react-router-dom';
 import { createStore, applyMiddleware } from 'redux';
@@ -9,21 +9,27 @@ import rootReducer from '../../reducers/index.js';
 import thunk from 'redux-thunk';
 
 describe('TriviaCard', () => {
-  it('should display a question and related info on render', () => {
-    const store = createStore(rootReducer, applyMiddleware(thunk));
+  let store, questionObj;
 
-    const questionObj = {
-            "category": "Entertainment: Film",
-            "type": "multiple",
-            "difficulty": "easy",
-            "question": "Which of the following movies was not based on a novel by Stephen King?",
-            "correct_answer": "The Thing",
-            "incorrect_answers": [
-                "Carrie",
-                "Misery",
-                "The Green Mile"
-            ]
-        }
+  beforeEach(() => {
+    store = createStore(rootReducer, applyMiddleware(thunk));
+
+    questionObj = {
+      "id": "909",
+      "category": "Entertainment: Film",
+      "type": "multiple",
+      "difficulty": "easy",
+      "question": "Which of the following movies was not based on a novel by Stephen King?",
+      "correct_answer": "The Thing",
+      "incorrect_answers": [
+          "Carrie",
+          "Misery",
+          "The Green Mile"
+      ]
+    }
+  });
+
+  it('should display a question and related info on render', () => {
 
     render(
       <Provider store={store}>
@@ -43,7 +49,7 @@ describe('TriviaCard', () => {
       </Provider>
     )
 
-    const button = screen.getByRole('button', {name: ""})
+    const button = screen.getByRole('button', {name: "Add"})
     const question = screen.getByText('Which of the following movies was not based on a novel by Stephen King?');
     const category = screen.getByText('Entertainment: Film');
     const difficulty = screen.getByText('easy');
@@ -57,5 +63,53 @@ describe('TriviaCard', () => {
     expect(correctAnswer).toBeInTheDocument();
     expect(incorrectAnswers).toBeInTheDocument();
   })
-  // test for fireEvent on add
+  it('should call addToGame when button is clicked', () => {
+    const mockAdd = jest.fn();
+    const mockCheck = jest.fn();
+
+    const mockQuestions = [
+      {
+        "id": "910",
+        "category": "Cats",
+        "type": "multiple",
+        "difficulty": "easy",
+        "question": "How many cats do I have?",
+        "correct_answer": "1",
+        "incorrect_answers": [
+            "0",
+            "2",
+            "3"
+        ]
+      }
+    ]
+
+    render(
+      <Provider store={ store }>
+        <BrowserRouter>
+          <TriviaCard
+          questions={mockQuestions}
+          question={'Which of the following movies was not based on a novel by Stephen King?'}
+          category={'Entertainment: Film'}
+          difficulty={'easy'}
+          correct={'The Thing'}
+          incorrect={["Carrie",
+          "Misery",
+          "The Green Mile"]}
+          everything={questionObj}
+          key={0}
+          addToGame={mockAdd}
+          checkForDuplicates={mockCheck}
+          />
+        </BrowserRouter>
+      </Provider>
+    )
+
+    const button = screen.getByRole('button', {name: 'Add'});
+
+    fireEvent.click(button);
+
+    expect(mockAdd).toBeCalledTimes(1);
+    expect(mockAdd).toBeCalledWith(questionObj);
+
+  })
 })
