4053e31c50268bc5891090708900b56853f84ffb
Taryn Martin <taryn.martin919@gmail.com>
2020-09-20 15:51:59 -0600

    Add user stories for testing

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,9 +1,42 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import { QuestionBar } from './QuestionBar';
+import { screen, render, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from '../../reducers/index.js';
+import thunk from 'redux-thunk';
+jest.mock('../../helpers/apiCalls');
+import { fetchQuestions } from '../../helpers/apiCalls.js';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('App', () => {
+  it('should show instructions on page load', () => {
+
+  });
+  it('should change the page when choose questions is selected', () => {
+
+  });
+  it('should show questions based on user search', async () => {
+
+  });
+  it('should change the page when your game button is selected', () => {
+
+  });
+  it('should return to the home page when the title is clicked', () => {
+
+  });
+  it('should return to the previous results when returning to choose questions page', () => {
+
+  });
+  it('should return to all questions on your questions page when returning', () => {
+
+  });
+  it('should display an error message if the user doesn\'t enter enough info for fetch', () => {
+
+  });
+  it('should display an error message if there are no results of fetch', () => {
+
+  });
+})
